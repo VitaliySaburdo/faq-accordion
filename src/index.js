1021aa477a890eb34d2df41f6d47cd503c012275
Vitaliy Saburdo <vsaburdo@gmail.com>
2024-08-05 12:26:28 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+const items = document.querySelectorAll('.faq__item');
+const list = document.querySelector('.faq__list');
 
+list.addEventListener('click', event => {
+  const selectedItem = event.target.closest('.faq__item');
+  if (!selectedItem) return;
+
+  const answer = selectedItem.querySelector('.faq__answer');
+  const itemImg = selectedItem.querySelector('.faq__icon');
+
+  itemImg.src = answer.classList.contains('show')
+    ? 'icon-plus.65996ed0.svg'
+    : 'icon-minus.19d080c8.svg';
+
+  answer.classList.toggle('show');
+
+  items.forEach(item => {
+    const itemAnswer = item.querySelector('.faq__answer');
+    const itemImg = item.querySelector('.faq__icon');
+
+    if (item !== selectedItem) {
+      itemAnswer.classList.remove('show');
+      itemImg.src = 'icon-plus.65996ed0.svg';
+    }
+  });
+});
